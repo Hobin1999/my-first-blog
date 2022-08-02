921fed0cd70c2da98ef27e88ab633c303dc959ed
Jack <Jack.Hobin@zoodigital.com>
2022-08-02 15:44:00 +0100

    third commit adding key back

--- mysite/settings.py
+++ mysite/settings.py
@@ -20,6 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-rv-e23^$=fhlk9t-)-26jr88edi+5e$^u7muk+%7w!xnzhv)(h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
